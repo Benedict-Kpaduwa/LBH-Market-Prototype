59c422f1c0d49cbfdf9ae310f4ff371604e486fa
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-12-01 23:16:46 +0100

    Starting the development of the Home page

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components"
+
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`
+
+const NavBar = () => {
+    return(
+        <div>
+            NavBar
+        </div>
+    )
+}
+
+export default NavBar
