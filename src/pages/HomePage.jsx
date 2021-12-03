d755a0ce8c0d4cf85d6d43f9cd53aa7beceaaef9
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-12-03 23:34:23 +0100

    Updated the HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Announcement from "../components/Announcement";
 import NavBar from "../components/NavBar";
+import Slider from "../components/Slider";
 
 const HomePage = () => {
     return(
         <div>
+            <Announcement />
             <NavBar />
+            <Slider />
         </div>
     )
 }
